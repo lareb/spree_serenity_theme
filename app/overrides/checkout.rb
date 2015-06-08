eadd693d05b0d20d8140e52659f703ad1363c511
dbwinger <dbwinger@gmail.com>
2015-06-08 18:59:25 -0400

    add po #

--- app/overrides/checkout.rb
+++ app/overrides/checkout.rb
@@ -4,4 +4,25 @@ Deface::Override.new(:virtual_path => 'spree/checkout/_delivery',
 Deface::Override.new(:virtual_path => 'spree/checkout/_confirm',
                     :name => "remove_images_confirm",
                     :remove => "td.item-image, thead th:first-child, colgroup col:first-child")
-
+Deface::Override.new(virtual_path: 'spree/checkout/_payment',
+                    name: 'add_po_number_input',
+                    insert_bottom: "[data-hook=\"checkout_payment_step\"]",
+                    text: "
+<p class='field' data-hook='po_number'>
+  <%= form.label :po_number, 'PO #' %><br/>
+  <%= form.text_field :po_number %>
+</p>"
+)
+Deface::Override.new(virtual_path: 'spree/shared/_order_details',
+  name: 'add_po_number',
+  insert_after: '#line-items',
+  text: "
+<% if @order.po_number.present? %>
+  <div class='row-fluid'>
+    <div class='span12 text-center'>
+      <b>PO #: </b>
+      <%= @order.po_number %>
+    </div>
+  </div>
+<% end %>
+")
